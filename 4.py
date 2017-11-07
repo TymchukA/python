04c85e5ac572c9fefb38b17593251baab5168d9d
TymchukA <31686062+TymchukA@users.noreply.github.com>
2017-11-07 09:42:24 +0200

    Update 4.py

--- 4.py
+++ 4.py
@@ -8,3 +8,26 @@ class Person:
     
     def ages(self):
         return int((datetime.now() - self.doB).days / 365.25)
+
+    
+    
+    ////from datetime import datetime, timedelta
+
+from Teacher import *
+
+ivi = Teacher("Ihor", "Ivaniuk", "25-07-1965", "Biology", "Proffesor", "PhD", 5000, 4)
+dst = Teacher("Dmytro", "Troyanivskii", "01-09-1959", "Biology", "Docent", "MD", 4500, 6)
+kvv = Teacher("Konstyantyn", "Voitovych", "12-12-1989", "Economics", "Asisstant", "BD", 3200, 2)
+vsd = Teacher("Valentyna", "Bobrova", "25-01-1970", "Economics", "Docent", "Ms", 4700, 5)
+arp = Teacher("Artem", "Prentkovych", "20-11-1955", "AM", "Proffesor", "PhD", 5670, 7)
+mlm = Teacher("Mykola", "Marchenko", "19-06-1990", "AM", "Assistant", "BD", 3200, 4)
+mak = Teacher("Maksym", "Kortenko", "02-02-1978", "AM", "Docent", "MD", 4890, 6)
+
+print(str(arp) + " age is " + str(arp.ages()))
+
+all_faculties = [ivi, dst, kvv, vsd, arp, mlm, mak]
+my_faculty = get_faculty(all_faculties, "AM")
+print("Sum of courses for all faculties: " + str(get_courses(all_faculties)))
+print("Sum of courses for my faculty: " + str(get_courses(my_faculty)))
+print("Average age of  all teachers: " + str(get_avr_age(all_faculties)))
+print("Average age of teachers from my faculty: " + str(get_avr_age(my_faculty)))
